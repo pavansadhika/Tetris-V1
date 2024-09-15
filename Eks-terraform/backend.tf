8828b8ac7085d08459d46a02da973271f6c3ee0b
pavansadhika <121276288+pavansadhika@users.noreply.github.com>
2024-09-15 18:33:19 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "ratmdatastore" # Replace with your actual S3 bucket name
+    bucket = "tetries-demo01" # Replace with your actual S3 bucket name
     key    = "eks/terraform.tfstate"
     region = "ap-south-1"
   }
